fe7969c453b2c1eeccd4eda442026cf4fd4024fc
iagoambrosio <iago_ambrosio@outlook.com>
2022-08-25 18:02:03 -0300

    Adicionando arquivo index para o projeto de api

--- api-node/index.js
+++ api-node/index.js
@@ -1,3 +1,31 @@
-const express = require('express')
-
-express.Route('/')
+/**Create mongodb-temporario
+ * sudo docker run --rm -it -p 27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=123456 -e MONGO_INITDB_DATABASE=db_js mongo 
+ */
+ const { application } = require('express')
+ const express = require('express')
+ const chamaExpress = express()
+ /** const app = require('') **/
+ 
+ // middlewares (meios de comunicação)
+ chamaExpress.use(
+     express.urlencoded(
+         {extended:true}
+     )
+ )
+ 
+ chamaExpress.use(express.json())
+ 
+ // ler json
+ 
+ //rotas, da principal as paralelas
+ chamaExpress.get('/', function(req,res){
+     
+     //response
+     res.json({
+         mensagem: 'olá'
+     })
+ 
+ })
+ 
+ // disponibilizar porta
+ chamaExpress.listen(3000)
