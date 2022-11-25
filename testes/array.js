da6e1b3ec4d123319efc79ca245c71e0435c122a
iago ambrosio <iago_ambrosio@outlook.com>
2022-11-25 19:11:05 -0300

    Adicionando arrays

---
+++ testes/array.js
@@ -0,0 +1,9 @@
+let alunos = ["João","Patricia","Carlos","Eleanor","Matilda","Elias"]
+
+let notas = ["10","3","7","0","9","5"]
+
+let media = [alunos,notas]
+
+console.log(media)
+console.log(media[1][4])
+console.log(media[0][1])
