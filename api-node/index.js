c398f839c3a087e9ab30fcbf669b4f43e040dd93
iago_ambrosio <iago_ambrosio@outlook.com>
2022-12-02 05:02:46 +0000

    Fazendo o primeiro POST

--- api-node/index.js
+++ api-node/index.js
@@ -10,71 +10,88 @@
 // colocando o modulo express dentro da variável express
 const express = require('express')
  // colocando a função express() dentro da variavel chamaExpress
-const chamaExpress = express()
+const Express = express()
 // chama o objeto exportado em Person.js
 const Person = require('./models/Person.js')
 // chama função para conectar no banco de dados 
 const mongodbservice = require('./models/Conectar.js')
 
-
 //Conecta no mongo
 mongodbservice()
 
-// ROTAS DA API
+//Versão API
+const versaoApi ='/api/v1'
 
-chamaExpress.post('/person',async (req,res) => {
+// middlewares (meios de comunicação)/ formas de ler os arquivos
 
-    console.log(this.req)
-    // req.body
-    const { name, salary, approved } = req.body
-
-    const person = {
-        name,
-        salary,
-        approved
-    }
-
-    // create
-
-    try{
-        //criando dados
-        await Person.create(person)
-
-        res.status(201).json({ message: 'Pessoa criada com sucesso no sistema'
-
-        })
-
-    } catch (error){
-        //tratando erros
-        res.status(500).json({error: error})
-    }
-
-
-})
-
- 
- // middlewares (meios de comunicação)/ formas de ler os arquivos
- /*chamaExpress.use(
+ Express.use(
      express.urlencoded(
          {extended:true}
      )
  )
- */
+
+//Rotas GET
+ Express.get(`${versaoApi}`, function(req,res){
+     //response
+     res.json({
+         mensagem: 'Gostosa'
+     })
+ 
+ })
+
+ Express.get(`${versaoApi}/person`, function(req,res){
+    //response
+    res.json({loga: "ok"})
+
+})
+
+// Rostas POST
+//teste /person
+// curl -X POST -H "Content-Type: application/json" -d '{"name" : "João", "salary" : 2000, "approved" : true }' https://devopers.ddns.net/api/v1/person
+Express.post(`${versaoApi}/person`,async (req,res) => {
+
+        // req.body
+        // {name: "Matheus", salary: 5000, approved: false }
+        
+        const {name,salary,approved} = req.body
+        const person = {
+            name,
+            salary,
+            approved
+        }
+        console.log(req.body)
+    
+        // create
+    
+        try{
+            //criando dados
+            await Person.create(person)
+    
+            res.status(201).json({ 
+                message: 'Pessoa criada com sucesso no sistema',
+                atributosPessoa: `${person}` 
+            })
+    
+        } catch (error){
+            //tratando erros
+            res.status(500).json({error: error})
+        }
+    
+})
+
+ 
+ 
+ 
 
  //chamaExpress.use(express.json())
  
  // ler json
  
- //rotas, da principal as paralelas
- chamaExpress.get('/', function(req,res){
-     //response
-     res.json({
-         mensagem: 'olá'
-     })
- 
- })
+
+
+
  
 
  // disponibilizar porta e fixa ip na em todas as interfaces
- chamaExpress.listen(3000)
- chamaExpress.bind('0.0.0.0')
+ Express.listen(3000)
+ Express.bind('0.0.0.0')
--- api-node/models/Person.js
+++ api-node/models/Person.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 // Instancia um objeto (Colections) "Pessoal" , o parametro 'Person' precisa ser escrito no singular 
-const Person = mongoose.model ('Person', {
+const Person = mongoose.model('Person', {
     name: String,
     salary: Number,
     approved: Boolean,
