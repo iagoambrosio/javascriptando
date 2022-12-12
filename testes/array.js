3c6c7825940a1f827d5b44e7fbcd7e6bcc9036d0
iago ambrosio <iago_ambrosio@outlook.com>
2022-12-12 20:24:44 -0300

    UPDATE

--- testes/array.js
+++ testes/array.js
@@ -7,3 +7,13 @@ let media = [alunos,notas]
 console.log(media)
 console.log(media[1][4])
 console.log(media[0][1])
+
+const array = [21,31]
+const arrayForEach = array.forEach(() => {
+    })
+const arrayMap = array.map((soma) => {
+return soma += 1
+})
+
+console.log(arrayForEach)
+console.log(arrayMap)
