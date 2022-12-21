fe1d4b9bdeb7e075ac76b35cac12e73b8d41ffe5
iagoambrosio <iago_ambrosio@outlook.com>
2022-12-21 15:52:17 -0300

    Ajeitando a realese da Dockerfile

---
+++ api-mongo/app/index.js
@@ -0,0 +1,37 @@
+//Requisitos para trabalhar com json e mongodb
+
+// colocando o modulo express dentro da variável express
+const express = require('express')
+
+// colocando a função express() dentro da variavel cExpress
+const Express = express()
+
+// chama função para conectar no banco de dados 
+const mongodbservice = require('./models/Conectar.js')
+
+//chama a função de personRoutes
+const personRoutes = require('./routes/person.js')
+
+// chama o modulo de caminho
+const path = require('path')
+
+//Conecta no mongo
+mongodbservice()
+// middlewares (meios de comunicação)/ formas de ler os arquivos
+Express.use(express.json())    // <==== parse request body as JSON, para as request não virem em branco
+
+// para o node etender responses em JSON
+Express.use(
+    express.urlencoded(
+        {extended:true}
+    )
+) 
+Express.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+Express.use(`/`, personRoutes)
+
+
+ // disponibilizar porta e fixa ip na em todas as interfaces
+ Express.listen(3000)
+ Express.bind('0.0.0.0')
---
+++ api-mongo/app/models/Conectar.js
@@ -0,0 +1,14 @@
+ //conecta ao mongodb e configura as mensagens de conexão
+ // chama o modulo para conexão no banco de dados mongo
+const mongoose = require('mongoose')
+
+
+function mongodbservice (){
+   mongoose.connect(
+      'mongodb://mongoadmin:123456@mongodb:27017/admin'
+   )
+   .then (console.log('Conectado a admin'))
+   .catch ((err) => console.log(err))
+}
+
+module.exports = mongodbservice
