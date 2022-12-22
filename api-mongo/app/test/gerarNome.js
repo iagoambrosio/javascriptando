76775d84bb98a8cce1ed19fc7811d21f69f0183c
iagoambrosio <iago_ambrosio@outlook.com>
2022-12-22 18:24:53 -0300

    Algumas correções

--- api-mongo/app/test/gerarNome.js
+++ api-mongo/app/test/gerarNome.js
@@ -1,50 +1,57 @@
 'use strict'
-function gerarNomes(rounds,tamanhoNome,minimoLetras){
-for (let i = 0; i < rounds; i++) {
-const consoante = ["b","c","d","f","g","h","i","j","k","l","m","n","p","q","r","s","t","v","x","y","z"]
-const vogal = ["a","e","i","o","u"]
-// função principal
-function mainGerarNome(){
-  const nomeGerado = {
-    "toUperCaseOnString" : null,
-    "onString" : null,
-    "onArray" : [],
-    "toUperCaseOnArray" : []
+function gerarNomes(rounds, tamanhoNome, minimoLetras) {
+  for (let i = 0; i < rounds; i++) {
+    const consoante = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"]
+    const vogal = ["a", "e", "i", "o", "u"]
+    // função principal
+    function mainGerarNome() {
+      const nomeGerado = {
+        "arrayescolhido": null,
+        "decisor": null,
+        "toUperCaseOnString": null,
+        "onString": null,
+        "onArray": [],
+        "toUperCaseOnArray": []
+      }
+      const arrayRandominize = (tamanhoNome, minimoLetras) => {
+        const resultado = Math.floor(Math.random() * (tamanhoNome - minimoLetras) + minimoLetras)
+        return resultado
+      }
+      function escolhArray() {
+        const decisor = arrayRandominize(100, 0)
+        nomeGerado.decisor = decisor
+        if (decisor >= 50) {
+          return consoante
+        } else { return vogal }
+      }
+      const arrayEscolhido = escolhArray()
+      nomeGerado.arrayescolhido = arrayEscolhido
+      function arrayQueRestou() {
+        if (arrayEscolhido == vogal) {
+          return consoante
+        } else { return vogal }
+      }
+      const segundoArray = arrayQueRestou()
+      //Determinando o tamanho da palavra
+      const escolhaPalavra = arrayRandominize(tamanhoNome, minimoLetras)
+      nomeGerado.tamanhodapalavra = escolhaPalavra
+      let i = 0
+      while (i < escolhaPalavra) {
+        const primeiraLetra = arrayEscolhido[arrayRandominize(arrayEscolhido.length, 0)]
+        nomeGerado.onArray.push(primeiraLetra)
+        ++i
+        if (i === escolhaPalavra) {
+          break
+        } else {
+          const segundaLetra = segundoArray[arrayRandominize(segundoArray.length, 0)]
+          nomeGerado.onArray.push(segundaLetra)
+        }
+        ++i
+      }
+      nomeGerado.onString = nomeGerado.onArray.toString()
+      return nomeGerado
+    }
+    console.log(mainGerarNome())
   }
-  const arrayRandominize = (max,min) => {
-    const resultado = Math.floor(Math.random()*(max - min) + min)
-    return resultado
 }
-function escolhArray(){
-  const decisor = arrayRandominize(100,0)
-  if (decisor >=50){
-    return consoante
-  } else { return vogal }
-}
-const arrayEscolhido = escolhArray()
-function arrayQueRestou(){if (arrayEscolhido === vogal){
-  return consoante 
-} else {return vogal}}
-const segundoArray = arrayQueRestou()
-   //Determinando o tamanho da palavra
-   const escolhaPalavra = arrayRandominize(tamanhoNome,minimoLetras)
-   let i = 0
-   while ( i < escolhaPalavra ){
-   const primeiraLetra = arrayEscolhido[arrayRandominize(arrayEscolhido.length,0)]
-   nomeGerado.onArray.push(primeiraLetra)
-   ++ i
-   if (i === escolhaPalavra){
-   break
-   }else{
-   const segundaLetra = segundoArray[arrayRandominize(segundoArray.length,0)]
-   nomeGerado.onArray.push(segundaLetra)
-   ++ i
-  }
-}
-nomeGerado.onString = nomeGerado.onArray.toString()
-return console.log(nomeGerado)
-}
-mainGerarNome()
-  } 
-}
-gerarNomes(2,12,4)
+gerarNomes(2, 12, 4)
