3e6b965f690653472ee543174f519cd79e9bd27e
iago Ambrosio <you@example.com>
2023-03-23 15:16:02 -0300

    Atualização de dockerfiles da api de conceito

---
+++ conceito/app/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) 
+const moduler = require('./routes/gets.js')
+const mainPaths = ["/main","/edit","/user"]
+// a nível de rota, o use recebe o caminho e manda para o Route do express
+app.use(mainPaths, moduler)
+app.post(mainPaths, moduler)
+app.put(mainPaths, moduler)
+app.options(mainPaths, moduler)
+app.delete(mainPaths, moduler)
+app.patch(mainPaths, moduler)
+// infra bind and listener
+app.listen(3000)
+app.bind("0.0.0.0")
